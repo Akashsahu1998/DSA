0819db038ac7ab4b431d473299240c2b8c9498e8
Akash <akkusahu559@gmail.com>
2021-07-27 08:19:22 +0530

    Representation of Binary Tree added into C++

---
+++ Tree/Binary Tree/01. Representation of Binary Tree.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node{
+	int key;
+	Node *left;
+	Node *right;
+	
+	Node(int k){
+		key = k;
+		left = NULL;
+		right = NULL;
+	}
+};
+
+int main(){
+	Node *root = new Node(10);
+	root->left = new Node(20);
+	root->right = new Node(30);
+	root->left->left = new Node(40);
+}
+
+
