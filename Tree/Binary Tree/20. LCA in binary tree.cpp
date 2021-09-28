c8ab6b314a9e442108856fc29d5b3b1fd7d2fb38
Akash <akkusahu559@gmail.com>
2021-09-28 08:02:45 +0530

    LCA of binary tree using DFS

--- Tree/Binary Tree/20. LCA in binary tree.cpp
+++ Tree/Binary Tree/20. LCA in binary tree.cpp
@@ -42,23 +42,38 @@ void levelOrderTraversal(Node *root){
 // Time Complexity = O(N), Space Complexity = O(H) where H is the height of the Binary Tree
 // In this solution we require three traversal of binary tree (0(N)+0(N)+0(N)) => 3(0(N)) => 0(N).
 
-bool findPath(Node* root, vector<Node*> &path, int node){
-    if(root == NULL) return false;
-    path.push_back(root);
-    if(root->key == node) return true;
-    if(findPath(root->left, path, node) || findPath(root->right, path, node)) return true;
-    path.pop_back();
-    return false;
-}
+//bool findPath(Node* root, vector<Node*> &path, int node){
+//    if(root == NULL) return false;
+//    path.push_back(root);
+//    if(root->key == node) return true;
+//    if(findPath(root->left, path, node) || findPath(root->right, path, node)) return true;
+//    path.pop_back();
+//    return false;
+//}
+//
+//Node* lowestCommonAncestor(Node* root, int p, int q) {
+//    vector<Node*> path1, path2;
+//    if(findPath(root, path1, p) == false || findPath(root, path2, q) == false) return NULL;
+//    int itr = 0;
+//    for(itr; itr < min(path1.size(), path2.size()); itr++){
+//        if(path1[itr] != path2[itr]) break;
+//    }
+//    return path1[itr-1];
+//}
+
+
+// 2nd Approach Using DFS
+// Time Complexity = O(N), Space Complexity = O(H) where H is the height of the Binary Tree
+// In this solution we require only one traversal of binary tree.
+// But in this approach both given key should be present.
 
 Node* lowestCommonAncestor(Node* root, int p, int q) {
-    vector<Node*> path1, path2;
-    if(findPath(root, path1, p) == false || findPath(root, path2, q) == false) return NULL;
-    int itr = 0;
-    for(itr; itr < min(path1.size(), path2.size()); itr++){
-        if(path1[itr] != path2[itr]) break;
-    }
-    return path1[itr-1];
+    if(root == NULL) return NULL;
+    if(root->key == p || root->key == q) return root;
+    Node* lcaLeft = lowestCommonAncestor(root->left, p, q);
+    Node* lcaRight = lowestCommonAncestor(root->right, p, q);
+    if(lcaLeft && lcaRight) return root;        
+    return lcaLeft ? lcaLeft : lcaRight;
 }
 
 
